61fb2adfb10f4c3defdb9a3571458e384e5cd7a9
MaxPoweur <adamrotard@laposte.net>
2018-06-28 16:34:06 +0100

    Fresh install

---
+++ src/App.tsx
@@ -0,0 +1,45 @@
+import * as React from 'react';
+import './App.css';
+
+import Button from '@material-ui/core/Button';
+
+import logo from './logo.svg';
+
+class App extends React.Component {
+
+    public state = {
+        response: ""
+    };
+
+    public componentDidMount() {
+        this.callApi()
+        .then(res => this.setState({ response: res.express }))
+        .catch(err => console.log(err));
+    }
+
+    public callApi = async () => {
+        const response = await fetch("/api/hello");
+        const body = await response.json();
+
+        if (response.status !== 200) throw Error(body.message);
+
+        return body;
+    }
+
+
+  public render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.tsx</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
